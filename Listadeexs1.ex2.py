3987d4ef51744df58e70632f36a13643bbaacf6f
Vitória <vitoria.martinsro.146@gmail.com>
2025-04-14 19:32:00 -0300

    Add files via upload

---
+++ Listadeexs1.ex2.py
@@ -0,0 +1,6 @@
+nome = input ("Qual o seu nome? ")
+sobrenome = input ("Qual o seu sobrenome? ")
+idade = input ("Qual a sua idade? ")
+idade = int(idade)
+
+print ("Meu nome, meu sobrenome e minha idade: ",sobrenome, nome, idade)
